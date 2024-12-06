bfbbbd3e15385ee9e8724a30af768b147bbc4d4d
Sean Gloumeau <sajgloumeau@protonmail.com>
2024-12-06 22:47:51 +0100

    Removed redundancy in hacky experiment for loop

--- phcad/experiments/onevall_classification.py
+++ phcad/experiments/onevall_classification.py
@@ -14,29 +14,29 @@ def run_onevall_exp_mvtec_ae(label, device=None):
     loss_function = SSIMLoss()
     weight_decay = 1e-5
 
+    train_size = 10000
+    cal_size = 0
+    tl_full, _ = mvtec_train_cal_dataloaders(
+        label, resize_px, crop_px, train_size, cal_size
+    )
+    train_size = 7500
+    cal_size = 2500
+    tl_partial, _ = mvtec_train_cal_dataloaders(
+        label, resize_px, crop_px, train_size, cal_size
+    )
     for seed in range(1, 6):
         logging.info(f"On seed {seed}")
 
         print("Training full")
         ae = AEMvTec()
         savename = f"mvtec-ae-full-{label}-{seed}"
-        train_size = 10000
-        cal_size = 0
-        trainloader, _ = mvtec_train_cal_dataloaders(
-            label, resize_px, crop_px, train_size, cal_size
-        )
         opt = torch.optim.Adam(ae.parameters(), weight_decay=weight_decay)
         sched = torch.optim.lr_scheduler.MultiStepLR(opt, milestones=(20, 40))
-        train(50, ae, loss_function, opt, sched, trainloader, savename, device=device)
+        train(50, ae, loss_function, opt, sched, tl_full, savename, device=device)
 
         print("Training partial")
         ae = AEMvTec()
         savename = f"mvtec-ae-partial-{label}-{seed}"
-        train_size = 7500
-        cal_size = 2500
-        trainloader, _ = mvtec_train_cal_dataloaders(
-            label, resize_px, crop_px, train_size, cal_size
-        )
-        opt = torch.optim.Adam(ae.parameters(), weight_decay=1e-5)
+        opt = torch.optim.Adam(ae.parameters(), weight_decay=weight_decay)
         sched = torch.optim.lr_scheduler.MultiStepLR(opt, milestones=(20, 40))
-        train(50, ae, loss_function, opt, sched, trainloader, savename, device=device)
+        train(50, ae, loss_function, opt, sched, tl_partial, savename, device=device)
