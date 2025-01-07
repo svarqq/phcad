da64ef8385936b60027e39fd4da16e97ff7f5aab
Sean Gloumeau <sajgloumeau@protonmail.com>
2025-01-07 18:18:56 +0100

    Completed fcdd impl

--- phcad/models/fcdd.py
+++ phcad/models/fcdd.py
@@ -3,15 +3,15 @@ from math import prod
 
 import torch
 import torch.nn.functional as F
-import torchvision
+from torchvision import models
 
 import matplotlib.pyplot as plt
 
 
-class FCDDMvTec(torch.nn.Module):
+class FCDD(torch.nn.Module):
     def __init__(self, input_2dshape):
-        super(FCDDMvTec, self).__init__()
-        vgg11_base = torchvision.models.vgg11_bn().features[:-8]
+        super(FCDD, self).__init__()
+        vgg11_base = models.vgg11_bn(weights="DEFAULT").features[:-8]
         vgg11_base[:15].requires_grad_(False)
 
         ksizes, strides, paddings = [], [], []
@@ -35,34 +35,37 @@ class FCDDMvTec(torch.nn.Module):
             except AttributeError:
                 pass
 
-        self.model = OrderedDict(
-            (
-                ("vgg11_bn_base", vgg11_base),
-                ("final_conv"),
-                torch.nn.Conv2D(512, 1, 1)(
-                    "receptive_upsample",
-                    ReceptiveUpsample(ksizes, strides, paddings),
-                ),
+        self.layers = torch.nn.Sequential(
+            OrderedDict(
+                (
+                    ("vgg11_bn_base", vgg11_base),
+                    ("final_conv", torch.nn.Conv2d(512, 1, 1)),
+                    (
+                        "receptive_upsample",
+                        ReceptiveUpsample(ksizes, strides, paddings),
+                    ),
+                )
             )
         )
 
+    def forward(self, x):
+        return self.layers(x)
+
 
 class ReceptiveUpsample(torch.nn.Module):
-    def __init__(self, kernel_sizes, strides, paddings, std=10):
-        # See https://distill.pub/2019/computing-receptive-fields/#solving-receptive-field-size for computing effective receptive field values
+    def __init__(self, kernel_sizes, strides, paddings, std=14):
+        # See https://distill.pub/2019/computing-receptive-fields/#solving-receptive-field-size
+        # for computing effective receptive field values
         super(ReceptiveUpsample, self).__init__()
         stride_products = [1] + [prod(strides[:i]) for i in range(1, len(strides))]
-        print(strides, stride_products)
         self.effective_padding = sum(
             [pad * sp for pad, sp in zip(paddings, stride_products)]
         )
-        print(paddings)
         self.effective_stride = prod(strides)
         self.effective_size = (
             sum([(k_sz - 1) * sp for k_sz, sp in zip(kernel_sizes, stride_products)])
             + 1
         )
-        print(self.effective_padding, self.effective_size, self.effective_stride)
 
         coordinates = torch.stack(
             torch.meshgrid(*(torch.arange(self.effective_size),) * 2, indexing="xy")
@@ -81,25 +84,18 @@ class ReceptiveUpsample(torch.nn.Module):
             ),
             0,
         )
-        # plt.imshow(gauss_weights)
-        # plt.show()
-        im = torch.unsqueeze(
-            torch.unsqueeze(
-                torch.tensor([[x + y for x in range(28)] for y in range(28)]), 0
-            ),
-            0,
-        ).to(torch.float)
-        print(im.shape)
-        im2 = F.conv_transpose2d(
-            im,
-            weight=gauss_weight,
+        self.weight = gauss_weight
+
+    def forward(self, x):
+        return F.conv_transpose2d(
+            x,
+            weight=self.weight,
             stride=self.effective_stride,
             padding=self.effective_padding,
         )
-        print(im2.shape)
-        plt.imshow(im2[0][0])
-        plt.show()
 
 
 if __name__ == "__main__":
-    model = FCDDMvTec((224, 224))
+    model = FCDD((224, 224))
+    breakpoint()
+    print(model)
