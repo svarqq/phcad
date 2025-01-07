Fix platt scaling for HSC

phcad/data_handling/utils.py
@@ -256,12 +256,15 @@ def mean_std(dataset: Subset, ae=False):
     transforms += [v2.ToImage(), v2.ToDtype(torch.get_default_dtype(), scale=True)]
     transforms = v2.Compose(transforms)
     idx_list = range(len(dataset))
-    if dataset.dataset.extend:
-        idx_list = (
-            np.argwhere(np.asarray(dataset.indices) < dataset.dataset.orig_len)
-            .flatten()
-            .tolist()
-        )
+    try:
+        if dataset.dataset.extend:
+            idx_list = (
+                np.argwhere(np.asarray(dataset.indices) < dataset.dataset.orig_len)
+                .flatten()
+                .tolist()
+            )
+    except AttributeError:
+        pass
     imgs = [transforms(dataset[i][0]) for i in idx_list]
     dataset.dataset.transform = tmp_transform
 

phcad/trainers/losses.py
@@ -41,7 +41,9 @@ class HSCLoss(_Loss):
         return F.binary_cross_entropy(p_estimates, labels)
 
     def get_logits(self, model_outputs, **kwargs):
-        return torch.vmap(pseudo_huber_score)(model_outputs)
+        p = self.get_pests(model_outputs)
+        logits = torch.log(p) - torch.log(1 - p)
+        return logits
 
     def get_pests(self, model_outputs, **kwargs):
         p_estimates = torch.vmap(rbf_with_pseudo_huber)(model_outputs)