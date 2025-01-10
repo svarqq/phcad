03bc9b9915ff2a22a25a872803e3f789a71901d9
Sean Gloumeau <sajgloumeau@protonmail.com>
2025-01-10 13:56:41 +0100

    Did a bit more fixing

--- phcad/test/evaluate.py
+++ phcad/test/evaluate.py
@@ -229,12 +229,13 @@ def evaluate_thresholding_segmentation_perturbation(
     for data, batch_target_masks in test_loader:
         with torch.device(device), torch.no_grad():
             data = data.to(device)
+
+            rsz_masks = F.resize(batch_target_masks, data.shape[-2:])
+            rsz_masks[rsz_masks >= 0.5] = 1
+            rsz_masks[rsz_masks < 0.5] = 0
             if detection_targets_for_loss:
                 targets = torch.stack(list(map(mask_to_class, batch_target_masks)))
             else:
-                rsz_masks = F.resize(batch_target_masks, data.shape[-2:])
-                rsz_masks[rsz_masks >= 0.5] = 1
-                rsz_masks[rsz_masks < 0.5] = 0
                 targets = rsz_masks
             targets = targets.to(device)
 
